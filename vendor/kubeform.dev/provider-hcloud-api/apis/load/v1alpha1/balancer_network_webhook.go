04b85d30a575b1b5efa77e4a5b64f282b8f52a6c
lgtm <1gtm@users.noreply.github.com>
2021-08-24 01:22:14 -0700

    Generate code for provider@v1.27.3-0.20210730143039-29d7eec9c077 gen@1aab85d (#8)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-hcloud-api/apis/load/v1alpha1/balancer_network_webhook.go
+++ vendor/kubeform.dev/provider-hcloud-api/apis/load/v1alpha1/balancer_network_webhook.go
@@ -20,9 +20,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"strings"
 
 	base "kubeform.dev/apimachinery/api/v1alpha1"
+	"kubeform.dev/provider-hcloud-api/util"
 
+	jsoniter "github.com/json-iterator/go"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -38,6 +41,14 @@ func (r *BalancerNetwork) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 var _ webhook.Validator = &BalancerNetwork{}
 
+var balancernetworkForceNewList = map[string]bool{
+	"/enable_public_interface": true,
+	"/ip":                      true,
+	"/load_balancer_id":        true,
+	"/network_id":              true,
+	"/subnet_id":               true,
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *BalancerNetwork) ValidateCreate() error {
 	return nil
@@ -45,6 +56,53 @@ func (r *BalancerNetwork) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *BalancerNetwork) ValidateUpdate(old runtime.Object) error {
+	if r.Spec.Resource.ID == "" {
+		return nil
+	}
+	newObj := r.Spec.Resource
+	res := old.(*BalancerNetwork)
+	oldObj := res.Spec.Resource
+
+	jsnitr := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		TagKey:                 "tf",
+		ValidateJsonRawMessage: true,
+		TypeEncoders:           GetEncoder(),
+		TypeDecoders:           GetDecoder(),
+	}.Froze()
+
+	byteNew, err := jsnitr.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	tempNew := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteNew, &tempNew)
+	if err != nil {
+		return err
+	}
+
+	byteOld, err := jsnitr.Marshal(oldObj)
+	if err != nil {
+		return err
+	}
+	tempOld := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteOld, &tempOld)
+	if err != nil {
+		return err
+	}
+
+	for key := range balancernetworkForceNewList {
+		keySplit := strings.Split(key, "/*")
+		length := len(keySplit)
+		checkIfAnyDif := false
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempOld, tempOld, tempNew)
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempNew, tempOld, tempNew)
+
+		if checkIfAnyDif && r.Spec.UpdatePolicy == base.UpdatePolicyDoNotDestroy {
+			return fmt.Errorf(`balancernetwork "%v/%v" immutable field can't be updated. To update, change spec.updatePolicy to Destroy`, r.Namespace, r.Name)
+		}
+	}
 	return nil
 }
 
