5d3e2f3951ef50486e0726a2a3bf41a8bfc48be1
1gtm <1gtm@appscode.com>
2022-03-01 09:35:50 +0000

    Cancel concurrent CI runs for same pr/commit
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- alljsonit.go
+++ alljsonit.go
@@ -41,170 +41,168 @@ type Data struct {
 	ResourceType string
 }
 
-var (
-	allJsonIt = map[schema.GroupVersionResource]Data{
-		{
-			Group:    "certificate.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "certificates",
-		}: {
-			JsonIt:       controllers.GetJSONItr(certificatev1alpha1.GetEncoder(), certificatev1alpha1.GetDecoder()),
-			ResourceType: "hcloud_certificate",
-		},
-		{
-			Group:    "firewall.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "firewalls",
-		}: {
-			JsonIt:       controllers.GetJSONItr(firewallv1alpha1.GetEncoder(), firewallv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_firewall",
-		},
-		{
-			Group:    "floating.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "ips",
-		}: {
-			JsonIt:       controllers.GetJSONItr(floatingv1alpha1.GetEncoder(), floatingv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_floating_ip",
-		},
-		{
-			Group:    "floating.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "ipassignments",
-		}: {
-			JsonIt:       controllers.GetJSONItr(floatingv1alpha1.GetEncoder(), floatingv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_floating_ip_assignment",
-		},
-		{
-			Group:    "load.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "balancers",
-		}: {
-			JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_load_balancer",
-		},
-		{
-			Group:    "load.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "balancernetworks",
-		}: {
-			JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_load_balancer_network",
-		},
-		{
-			Group:    "load.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "balancerservices",
-		}: {
-			JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_load_balancer_service",
-		},
-		{
-			Group:    "load.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "balancertargets",
-		}: {
-			JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_load_balancer_target",
-		},
-		{
-			Group:    "managed.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "certificates",
-		}: {
-			JsonIt:       controllers.GetJSONItr(managedv1alpha1.GetEncoder(), managedv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_managed_certificate",
-		},
-		{
-			Group:    "network.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "networks",
-		}: {
-			JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_network",
-		},
-		{
-			Group:    "network.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "routes",
-		}: {
-			JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_network_route",
-		},
-		{
-			Group:    "network.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "subnets",
-		}: {
-			JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_network_subnet",
-		},
-		{
-			Group:    "rdns.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "rdns",
-		}: {
-			JsonIt:       controllers.GetJSONItr(rdnsv1alpha1.GetEncoder(), rdnsv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_rdns",
-		},
-		{
-			Group:    "server.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "servers",
-		}: {
-			JsonIt:       controllers.GetJSONItr(serverv1alpha1.GetEncoder(), serverv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_server",
-		},
-		{
-			Group:    "server.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "networks",
-		}: {
-			JsonIt:       controllers.GetJSONItr(serverv1alpha1.GetEncoder(), serverv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_server_network",
-		},
-		{
-			Group:    "snapshot.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "snapshots",
-		}: {
-			JsonIt:       controllers.GetJSONItr(snapshotv1alpha1.GetEncoder(), snapshotv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_snapshot",
-		},
-		{
-			Group:    "ssh.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "keys",
-		}: {
-			JsonIt:       controllers.GetJSONItr(sshv1alpha1.GetEncoder(), sshv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_ssh_key",
-		},
-		{
-			Group:    "uploaded.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "certificates",
-		}: {
-			JsonIt:       controllers.GetJSONItr(uploadedv1alpha1.GetEncoder(), uploadedv1alpha1.GetDecoder()),
-			ResourceType: "hcloud_uploaded_certificate",
-		},
-		{
-			Group:    "volume.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "volumes",
-		}: {
-			JsonIt:       controllers.GetJSONItr(volumev1alpha1.GetEncoder(), volumev1alpha1.GetDecoder()),
-			ResourceType: "hcloud_volume",
-		},
-		{
-			Group:    "volume.hcloud.kubeform.com",
-			Version:  "v1alpha1",
-			Resource: "attachments",
-		}: {
-			JsonIt:       controllers.GetJSONItr(volumev1alpha1.GetEncoder(), volumev1alpha1.GetDecoder()),
-			ResourceType: "hcloud_volume_attachment",
-		},
-	}
-)
+var allJsonIt = map[schema.GroupVersionResource]Data{
+	{
+		Group:    "certificate.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "certificates",
+	}: {
+		JsonIt:       controllers.GetJSONItr(certificatev1alpha1.GetEncoder(), certificatev1alpha1.GetDecoder()),
+		ResourceType: "hcloud_certificate",
+	},
+	{
+		Group:    "firewall.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "firewalls",
+	}: {
+		JsonIt:       controllers.GetJSONItr(firewallv1alpha1.GetEncoder(), firewallv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_firewall",
+	},
+	{
+		Group:    "floating.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "ips",
+	}: {
+		JsonIt:       controllers.GetJSONItr(floatingv1alpha1.GetEncoder(), floatingv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_floating_ip",
+	},
+	{
+		Group:    "floating.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "ipassignments",
+	}: {
+		JsonIt:       controllers.GetJSONItr(floatingv1alpha1.GetEncoder(), floatingv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_floating_ip_assignment",
+	},
+	{
+		Group:    "load.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "balancers",
+	}: {
+		JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_load_balancer",
+	},
+	{
+		Group:    "load.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "balancernetworks",
+	}: {
+		JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_load_balancer_network",
+	},
+	{
+		Group:    "load.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "balancerservices",
+	}: {
+		JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_load_balancer_service",
+	},
+	{
+		Group:    "load.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "balancertargets",
+	}: {
+		JsonIt:       controllers.GetJSONItr(loadv1alpha1.GetEncoder(), loadv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_load_balancer_target",
+	},
+	{
+		Group:    "managed.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "certificates",
+	}: {
+		JsonIt:       controllers.GetJSONItr(managedv1alpha1.GetEncoder(), managedv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_managed_certificate",
+	},
+	{
+		Group:    "network.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "networks",
+	}: {
+		JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_network",
+	},
+	{
+		Group:    "network.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "routes",
+	}: {
+		JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_network_route",
+	},
+	{
+		Group:    "network.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "subnets",
+	}: {
+		JsonIt:       controllers.GetJSONItr(networkv1alpha1.GetEncoder(), networkv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_network_subnet",
+	},
+	{
+		Group:    "rdns.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "rdns",
+	}: {
+		JsonIt:       controllers.GetJSONItr(rdnsv1alpha1.GetEncoder(), rdnsv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_rdns",
+	},
+	{
+		Group:    "server.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "servers",
+	}: {
+		JsonIt:       controllers.GetJSONItr(serverv1alpha1.GetEncoder(), serverv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_server",
+	},
+	{
+		Group:    "server.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "networks",
+	}: {
+		JsonIt:       controllers.GetJSONItr(serverv1alpha1.GetEncoder(), serverv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_server_network",
+	},
+	{
+		Group:    "snapshot.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "snapshots",
+	}: {
+		JsonIt:       controllers.GetJSONItr(snapshotv1alpha1.GetEncoder(), snapshotv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_snapshot",
+	},
+	{
+		Group:    "ssh.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "keys",
+	}: {
+		JsonIt:       controllers.GetJSONItr(sshv1alpha1.GetEncoder(), sshv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_ssh_key",
+	},
+	{
+		Group:    "uploaded.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "certificates",
+	}: {
+		JsonIt:       controllers.GetJSONItr(uploadedv1alpha1.GetEncoder(), uploadedv1alpha1.GetDecoder()),
+		ResourceType: "hcloud_uploaded_certificate",
+	},
+	{
+		Group:    "volume.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "volumes",
+	}: {
+		JsonIt:       controllers.GetJSONItr(volumev1alpha1.GetEncoder(), volumev1alpha1.GetDecoder()),
+		ResourceType: "hcloud_volume",
+	},
+	{
+		Group:    "volume.hcloud.kubeform.com",
+		Version:  "v1alpha1",
+		Resource: "attachments",
+	}: {
+		JsonIt:       controllers.GetJSONItr(volumev1alpha1.GetEncoder(), volumev1alpha1.GetDecoder()),
+		ResourceType: "hcloud_volume_attachment",
+	},
+}
 
 func getJsonItAndResType(gvr schema.GroupVersionResource) Data {
 	return allJsonIt[gvr]
